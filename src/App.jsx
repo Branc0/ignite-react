0e0712148a7c839c5efd63ea3e18a0800200ee9f
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2021-05-29 15:26:58 -0300

    feat: creates first componentes, and introduces react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <h1>Hello world</h1>
+    return (
+        <>
+            <h1>Hello world</h1>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: ''
+}
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repostiórios</h1>
+            <ul>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem />
+                <RepositoryItem />
+                <RepositoryItem />
+            </ul>
+        </section>
+    )
+}
